7afe01ee080dec768075fe5ee84c955faf6b8364
Miles Zimmerman <miles@maztek.io>
2017-06-12 17:53:48 -0700

    Adding firebase/emberfire. Adding CSP. Adding DotEnv. Remove welcome page. Basic setup.

---
+++ config/initializers/content-security-policy.js
@@ -0,0 +1,30 @@
+module.exports = (process) => {
+  return {
+    'default-src': [
+      "'self'"
+    ],
+    'script-src': [
+      "'self'",
+      "'unsafe-eval'",
+      "'unsafe-inline'",
+      "apis.google.com",
+    ],
+    'frame-src': [
+      "'self'",
+      "https://*.firebaseapp.com",
+    ],
+    'connect-src': [
+      "'self'",
+      "wss://*.firebaseio.com",
+      "https://*.googleapis.com",
+    ],
+    'style-src': [
+      "'self'",
+      "'unsafe-inline'"
+    ],
+    'img-src': [
+      "'self'",
+      "*.appspot.com",
+    ]
+  };
+}
